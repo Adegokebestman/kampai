c3796f2c51ca0599d53397452865e79c12957ae1
Adegoke <adegokebestman@gmail.com>
2023-02-26 14:20:15 +0100

    Update

--- src/pages/Roles.jsx
+++ src/pages/Roles.jsx
@@ -19,7 +19,7 @@ const handleRoleAssignment = (role) => {
 	setSelectedRole(role);
 	// navigate(`/signup?role=${role}`);
 	const roleLink = {
-		"buyer" : "http://localhost:3000/signup",
+		"buyer" :"https://rad-fenglisu-2adbc8.netlify.app//signup",
 		"supplier": 'http://localhost:3001/signup',
 		"truck": 'http://localhost:3002/signup'
 	}
